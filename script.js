f46c1950fa11029d7dfe995018041beeefa1c462
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-03-18 21:22:06 +0100

    Implement Change Grid functionality
    Added class to grid size element
    
    Creating new grid updates current grid
    
    Implemented removal of grid, it is called before
    creation of new grid
    
    Implemented prompting and creation of new grid on
    click of a button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function createGrid(size) {
     let container = document.querySelector(".container");
+    let current = document.querySelector(".current");
+    current.textContent = `${size}x${size}`
+
+    removeGrid()
 
     for (let j = 0; j < size; j++) {
         let row = document.createElement("div")
@@ -16,9 +20,30 @@ function createGrid(size) {
     document.querySelectorAll(".grid").forEach(element => {
     element.addEventListener("mouseover",function(event){
         element.classList.add("dark")
-     });
+        });
+    });
+};
+
+function removeGrid() {
+    document.querySelectorAll(".grid").forEach(element => {
+        element.remove();
+    });
+};
+
+let changeGrid = document.querySelector(".button-grid");
+changeGrid.addEventListener("click",() =>{
+    let change = Number(prompt("How big should one side be?"));
+    change = Number(change);
+    if (change < 1) {
+        alert("Min: 1");
+    } else if (change > 100) {
+        alert("Max: 100");
+    } else if (Number.isInteger(change)) {
+        let text;
+        createGrid(change);
+    } else {
+        alert("Give me a number");
+    }
 });
-}
 
 createGrid(16);
-
